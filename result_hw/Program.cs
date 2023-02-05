706400b5f9b7d3aa83957035da2039564f3e66f9
Kristina <zkvzkvzkv@yandex.ru>
2023-02-05 22:56:15 +0300

    создана программа по условию задачи

--- result_hw/Program.cs
+++ result_hw/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+
+string[] array = { "Hello", "2", "world", ":-" };
+// string[] array = {"1234", "1567", "-2", "computer science"};
+// string[] array = {"Russia", "Denmark", "Kazan"};
+
+string[] result = new string[array.Length];
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        result[count] = array[i];
+        count++;
+    }
+}
+Console.WriteLine(string.Join(" ",result));
+Console.ReadKey();
